646efabf644628421d8e0d877a69e22b76c4b6a5
uxdsrini <uxdsrini@gmail.com>
2024-12-13 23:25:13 +0530

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+    import ReactDOM from 'react-dom';
+    import App from './App.jsx';
+    import LandingPage from './LandingPage.jsx';
+    import LoginPage from './LoginPage.jsx';
+    import './index.css';
+    import { AuthProvider } from './AuthContext.jsx';
+
+    ReactDOM.render(
+      <React.StrictMode>
+        <AuthProvider>
+          <LandingPage />
+        </AuthProvider>
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
